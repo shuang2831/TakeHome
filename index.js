9448270cbd2a232229767716987dd24d3e594ffa
shuang2831 <shuang2831@gmail.com>
2022-06-25 10:59:02 -0400

    Install and Initialize Apollo + GraphQL Client

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const client = new ApolloClient({
+  uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+  cache: new InMemoryCache(),
+});
+
+const AppWithApollo = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent(appName, () => AppWithApollo);
